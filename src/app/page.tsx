ad65621efe4d694b45519c685e7a6b74f6feef43
shibaram-brilliantlink <shibaram@brilliantlink.tech>
2024-12-24 17:54:49 +0530

    Added MUI library for consistent design system and faster development.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import HeroSection from "@/components/HeroSection";
+
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center text-2xl">
-      Home page
-    </div>
+    <main className="">
+      <div>
+        <HeroSection />
+      </div>
+    </main>
   );
 }
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,34 @@
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import Link from "next/link";
+
 export default function Header() {
   return (
-    <div className=" h-[100px] bg-yellow-200 flex items-center justify-center text-2xl">
-      Header section
-    </div>
+    <header className="">
+      <div className="top bg-[#ebebeb] h-[75px] text-neutral-600 md:h-[40px] flex flex-col items-center justify-center gap-2 md:flex-row md:justify-between">
+        <div className="flex items-center">
+          <LocationOnIcon className="hover:text-[#ffb600] cursor-pointer transition duration-300 ease-in-out" />{" "}
+          <span>9051 Apex Builders Co., India</span>
+        </div>
+        <div className="flex gap-2 hover:cursor-pointer">
+          <Link href="#" target="_blank">
+            <FacebookIcon className="hover:text-[#ffb600] transition duration-300 ease-in-out" />
+          </Link>
+          <Link href="#" target="_blank">
+            <InstagramIcon className="hover:text-[#ffb600] transition duration-300 ease-in-out" />
+          </Link>
+          <Link href="#" target="_blank">
+            <XIcon className="hover:text-[#ffb600] transition duration-300 ease-in-out" />
+          </Link>
+          <Link href="#" target="_blank">
+            <LinkedInIcon className="hover:text-[#ffb600] transition duration-300 ease-in-out text-[26px]" />
+          </Link>
+        </div>
+      </div>
+      <div className="h-40 bg-[#ffb600]"> dfdf</div>
+    </header>
   );
 }
