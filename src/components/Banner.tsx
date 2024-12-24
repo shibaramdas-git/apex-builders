f0148102aa8adf55fa0927bf9fee4864253c93d9
shibaram-brilliantlink <shibaram@brilliantlink.tech>
2024-12-24 11:00:24 +0530

    Banner section basic styles

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,19 +3,21 @@ type IBannerProps = {
   breadScrumb: string;
   bgImageSrc: string;
 };
+
 export default function Banner(props: IBannerProps) {
-  const { bgImageSrc } = props;
-  const bgImage = bgImageSrc ? "bg-[url(`${bgImageSrc}`)]" : "";
-  console.log(bgImage);
+  const { bgImageSrc, heading, breadScrumb } = props;
 
   return (
-    <section className="c_banner bg-[url('/buildings/photodune-5893828-downtown-dubai-m-1024x787.jpg')] bg-no-repeat bg-cover bg-bottom">
+    <section
+      className="c_banner bg-no-repeat bg-cover bg-bottom"
+      style={{ backgroundImage: `url(${bgImageSrc})` }}
+    >
       <div className="text-white uppercase border border-white flex flex-col items-center justify-center h-[300px] bg-[#000000be]">
         <h1 className="text-2xl md:text-5xl font-black mb-1">
-          {props.heading || "Heading"}
+          {heading || "Heading"}
         </h1>
         <p className="text-xs md:text-lg font-bold">
-          {props.breadScrumb || "HOME / SERVICES / service 01"}
+          {breadScrumb || "HOME / SERVICES / service 01"}
         </p>
       </div>
     </section>
