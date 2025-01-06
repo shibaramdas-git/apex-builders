f6217edf7ca088b9361d0f70c8ffc523e56ad2f9
shibaramdas-git <shibaram.daseng@gmail.com>
2025-01-06 18:08:17 +0530

    #schema added options for category

--- src/sanity/schemaTypes/documents/category.ts
+++ src/sanity/schemaTypes/documents/category.ts
@@ -30,10 +30,40 @@ export default defineType({
         list: [
           { title: "Article", value: "article" },
           { title: "Project", value: "project" },
-          { title: "Both", value: "both" },
         ], // Dropdown selection
         layout: "dropdown", // Optional: "radio" or "dropdown"
       },
+    }),
+    defineField({
+      name: "references",
+      title: "Reference",
+      type: "array",
+      hidden: ({ parent }) => parent?.type === undefined,
+      of: [
+        {
+          type: "reference",
+          to: [{ type: "article" }, { type: "project" }],
+          options: {
+            filter: ({ parent }) => {
+              if (!parent?.type)
+                return {
+                  filter: "",
+                };
+              if (!Array.isArray(parent) && parent?.type === "article") {
+                return {
+                  filter: '_type == "article"',
+                };
+              }
+              if (!Array.isArray(parent) && parent?.type === "project") {
+                return {
+                  filter: '_type == "project"',
+                };
+              }
+              return {}; // No filtering if no type
+            },
+          },
+        },
+      ],
     }),
   ],
 });
