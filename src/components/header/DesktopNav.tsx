4c952ff1be8361f9d3f7c8c6a2596d4756db4d0a
shibaramdas-git <shibaram.daseng@gmail.com>
2025-01-11 11:22:21 +0530

    #design implement basic dropdown design.(then make it reusable)

--- src/components/header/DesktopNav.tsx
+++ src/components/header/DesktopNav.tsx
@@ -6,14 +6,14 @@ export default function DesktopNav() {
     <div className="hidden container md:flex justify-between w-full text-white font-mont font-semibold relative">
       <ul className="flex gap-5">
         <li className="py-4  hover:text-yellow">
-          <Link href="/">Home</Link>
+          <Link href="/">HOME</Link>
         </li>
         {/* Dropdown for About us */}
-        <li className="dropdown relative group">
-          <button className="hover:text-yellow uppercase py-4">
+        <li className="dropdown relative group py-4">
+          <button className="hover:text-yellow uppercase">
             Company <IoIosArrowDown className="inline group-hover:rotate-180" />
           </button>
-          <ul className="hidden group-hover:flex flex-col absolute bg-white min-w-[200px] px-6 text-black text-xs border-t-2 border-yellow">
+          <ul className="hidden group-hover:flex flex-col absolute top-full bg-white min-w-[200px] px-6 text-black text-xs border-t-2 border-yellow">
             <li className="">
               <Link
                 href="/about"
@@ -49,124 +49,99 @@ export default function DesktopNav() {
           </ul>
         </li>
         <li className="py-4 hover:text-yellow">
-          <Link href="/projects">Projects</Link>
+          <Link href="/projects">PROJECTS</Link>
         </li>
         {/* All 6 services menu */}
-        <li className="py-4 hover:text-yellow">
-          <div className="group">
+        <li className="hover:text-yellow">
+          <div className="group py-4">
             <button className="hover:text-yellow uppercase">
               SERVICES
               <IoIosArrowDown className="inline group-hover:rotate-180" />
             </button>
-            <div className="hidden absolute top-[60px] left-[1rem] group-hover:block bg-white p-4 w-full">
+            <ul className="hidden group-hover:flex flex-col absolute top-full bg-white min-w-[200px] px-6 text-black text-xs border-t-2 border-yellow">
+              <li className="">
+                <Link
+                  href="/services"
+                  className=" block py-4 border-b border-zinc-300 hover:text-yellow "
+                >
+                  About Us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/about/team"
+                  className=" block py-4 border-b border-zinc-300 hover:text-yellow "
+                >
+                  Our Team
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/about/career"
+                  className=" block py-4 border-b border-zinc-300 hover:text-yellow "
+                >
+                  Career
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/about/faq"
+                  className=" block py-4 hover:text-yellow "
+                >
+                  FAQs
+                </Link>
+              </li>
+            </ul>
+            {/* <div className="hidden absolute left-[1rem] top-full group-hover:block bg-white p-4 container  border-t-2 border-yellow">
               <div className="border border-black flex h-full py-4 w-full">
-                <div className="flex flex-wrap items-center justify-center gap-4 lg:w-[70%]">
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                  <div className="flex border border-black w-[300px]">
-                    <div className="bg-slate-200 w-20">
-                      <img src="#" alt="" />
-                      image
-                    </div>
-                    <div>
-                      <h4>Interior design</h4>
-                      <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    </div>
-                  </div>
-                </div>
-                <div className="border-l border-l-gray hidden lg:flex justify-center items-center flex-1">
-                  <img src="https://dummyjson.com/image/230x230" alt="df" />
+                <div className="flex items-center justify-center gap-4 lg:w-[70%]">
+                  <ul className=" bg-white min-w-[200px] px-6 text-black text-xs">
+                    <li className="">
+                      <Link
+                        href="/about"
+                        className=" block py-4 border-b border-zinc-300 hover:text-yellow "
+                      >
+                        About Us
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/about/team"
+                        className=" block py-4 border-b border-zinc-300 hover:text-yellow "
+                      >
+                        Our Team
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/about/career"
+                        className=" block py-4 border-b border-zinc-300 hover:text-yellow "
+                      >
+                        Career
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        href="/about/faq"
+                        className=" block py-4 hover:text-yellow "
+                      >
+                        FAQs
+                      </Link>
+                    </li>
+                  </ul>
                 </div>
               </div>
-            </div>
+            </div> */}
           </div>
         </li>
         {/* Articles with categories and recent articles */}
-        <li className="py-4 hover:text-yellow">
-          <div className="group">
+        <li className="hover:text-yellow">
+          <div className="group py-4">
             <button className="hover:text-yellow uppercase">
               ARTICLES
               <IoIosArrowDown className="inline group-hover:rotate-180" />
             </button>
-            <div className="hidden absolute top-[60px] left-[1rem] group-hover:block bg-white p-4 w-full">
+            <div className="hidden absolute top-full left-[1rem] group-hover:block bg-white p-4 w-full  border-t-2 border-yellow">
               <div className="border border-black flex justify-between h-full py-4 w-full">
                 <div className="bg-slate-200">
                   <h4>All categories</h4>
@@ -227,11 +202,11 @@ export default function DesktopNav() {
           </div>
         </li>
         <li className="py-4 hover:text-yellow">
-          <Link href="/contact">Contact</Link>
+          <Link href="/contact">CONTACT</Link>
         </li>
       </ul>
       <div className="py-4">
-        <Link href="/studio">Studio</Link>
+        <Link href="/studio">STUDIO</Link>
       </div>
     </div>
   );
--- src/components/header/MobileNav.tsx
+++ src/components/header/MobileNav.tsx
@@ -1,8 +1,12 @@
+import Dropdown from "./Dropdown";
+
 export default function MobileNav() {
   return (
     <div className="container flex md:hidden items-center justify-between text-white w-full ">
-      <div>//#//Apex MobileNav dropdown</div>
-      <div>Menu</div>
+      <div>//#//Apex </div>
+
+      <Dropdown />
+      <Dropdown />
     </div>
   );
 }
